40e2deebc8c807d5e72a14fe83978d84b8f523da
Maverick1982 <>
2026-08-16 10:58:20 +0200

    refactor(generic): align GenericDriver with usbhid-ups.c
    Added fallback paths for basic HID capabilities (UPS.Input.Voltage, UPS.Battery.Voltage, UPS.Output.PercentLoad, etc.) to handle UPS brands that don't match strict Eaton/CyberPower schema but follow basic USB HID specs.

--- lib/USBHostUPS/src/GenericDriver.cpp
+++ lib/USBHostUPS/src/GenericDriver.cpp
@@ -85,33 +85,72 @@ void GenericDriver::decodeReport(USBHostUPS* host, uint8_t report_id, uint8_t re
 
     static const Mapping mappings[] = {
         { "UPS.PowerSummary.PresentStatus.ACPresent", [](UPSData& d, double v) { d.acPresent = v != 0; } },
+        { "UPS.PowerSummary.ACPresent", [](UPSData& d, double v) { d.acPresent = v != 0; } },
         { "UPS.PowerSummary.PresentStatus.Discharging", [](UPSData& d, double v) { d.discharging = v != 0; } },
+        { "UPS.PowerSummary.Discharging", [](UPSData& d, double v) { d.discharging = v != 0; } },
         { "UPS.PowerSummary.PresentStatus.Charging", [](UPSData& d, double v) { d.charging = v != 0; } },
+        { "UPS.PowerSummary.Charging", [](UPSData& d, double v) { d.charging = v != 0; } },
         { "UPS.PowerSummary.PresentStatus.BelowRemainingCapacityLimit", [](UPSData& d, double v) { d.belowRemainingCapacityLimit = v != 0; } },
+        { "UPS.PowerSummary.BelowRemainingCapacityLimit", [](UPSData& d, double v) { d.belowRemainingCapacityLimit = v != 0; } },
         { "UPS.PowerSummary.PresentStatus.NeedReplacement", [](UPSData& d, double v) { d.needReplacement = v != 0; } },
+        { "UPS.PowerSummary.NeedReplacement", [](UPSData& d, double v) { d.needReplacement = v != 0; } },
         { "UPS.PowerSummary.PresentStatus.Overload", [](UPSData& d, double v) { d.overload = v != 0; } },
+        { "UPS.PowerSummary.Overload", [](UPSData& d, double v) { d.overload = v != 0; } },
         { "UPS.PowerSummary.PresentStatus.ShutdownImminent", [](UPSData& d, double v) { d.shutdownImminent = v != 0; } },
+        { "UPS.PowerSummary.ShutdownImminent", [](UPSData& d, double v) { d.shutdownImminent = v != 0; } },
         { "UPS.PowerSummary.PresentStatus.CommunicationLost", [](UPSData& d, double v) { d.communicationLost = v != 0; } },
+        { "UPS.PowerSummary.CommunicationLost", [](UPSData& d, double v) { d.communicationLost = v != 0; } },
         
         { "UPS.PowerConverter.Input.Voltage", [](UPSData& d, double v) { d.inputVoltage = v; } },
+        { "UPS.Input.Voltage", [](UPSData& d, double v) { d.inputVoltage = v; } },
+        { "UPS.Flow.Voltage", [](UPSData& d, double v) { d.inputVoltage = v; } },
+        
         { "UPS.PowerConverter.Output.Voltage", [](UPSData& d, double v) { d.outputVoltage = v; } },
+        { "UPS.Output.Voltage", [](UPSData& d, double v) { d.outputVoltage = v; } },
         
         { "UPS.PowerSummary.Voltage", [](UPSData& d, double v) { d.batteryVoltage = v; } },
+        { "UPS.BatterySystem.Voltage", [](UPSData& d, double v) { d.batteryVoltage = v; } },
+        { "UPS.Battery.Voltage", [](UPSData& d, double v) { d.batteryVoltage = v; } },
+        
         { "UPS.PowerSummary.RemainingCapacity", [](UPSData& d, double v) { d.remainingCapacity = v > 100 ? 100 : (uint8_t)v; } },
         { "UPS.PowerSummary.RemainingCapacityLimit", [](UPSData& d, double v) { d.remainingCapacityLimit = (uint8_t)v; } },
         { "UPS.PowerSummary.RunTimeToEmpty", [](UPSData& d, double v) { d.runTimeToEmpty = (uint32_t)v; } },
+        { "UPS.Battery.RunTimeToEmpty", [](UPSData& d, double v) { d.runTimeToEmpty = (uint32_t)v; } },
         
         { "UPS.PowerSummary.PercentLoad", [](UPSData& d, double v) {
             d.load = (uint8_t)v;
             if (d.configActivePower > 0) d.realPower = (uint16_t)(((uint32_t)d.configActivePower * (uint32_t)v) / 100);
             else if (d.configApparentPower > 0) d.realPower = (uint16_t)(((uint32_t)d.configApparentPower * 60 * (uint32_t)v) / 10000);
         }},
+        { "UPS.Output.PercentLoad", [](UPSData& d, double v) {
+            d.load = (uint8_t)v;
+            if (d.configActivePower > 0) d.realPower = (uint16_t)(((uint32_t)d.configActivePower * (uint32_t)v) / 100);
+            else if (d.configApparentPower > 0) d.realPower = (uint16_t)(((uint32_t)d.configApparentPower * 60 * (uint32_t)v) / 10000);
+        }},
+        
         { "UPS.BatterySystem.Battery.DesignCapacity", [](UPSData& d, double v) { d.designCapacity = (uint8_t)(v / 3600.0); } },
         { "UPS.BatterySystem.Battery.FullChargeCapacity", [](UPSData& d, double v) { d.fullChargeCapacity = (uint8_t)(v / 3600.0); } },
+        
         { "UPS.Flow.ConfigActivePower", [](UPSData& d, double v) { d.configActivePower = (uint16_t)v; } },
+        { "UPS.PowerConverter.ConfigActivePower", [](UPSData& d, double v) { d.configActivePower = (uint16_t)v; } },
+        { "UPS.Output.ConfigActivePower", [](UPSData& d, double v) { d.configActivePower = (uint16_t)v; } },
+        
         { "UPS.Flow.ConfigApparentPower", [](UPSData& d, double v) { d.configApparentPower = (uint16_t)v; } },
+        
         { "UPS.PowerSummary.ConfigVoltage", [](UPSData& d, double v) { d.configVoltage = (uint16_t)v; } },
-        { "UPS.Flow.ConfigVoltage", [](UPSData& d, double v) { d.configVoltage = (uint16_t)v; } }
+        { "UPS.Flow.ConfigVoltage", [](UPSData& d, double v) { d.configVoltage = (uint16_t)v; } },
+        { "UPS.Input.ConfigVoltage", [](UPSData& d, double v) { d.configVoltage = (uint16_t)v; } },
+        
+        { "UPS.PowerConverter.Output.ConfigVoltage", [](UPSData& d, double v) { d.outputVoltageNominal = (uint16_t)v; } },
+        { "UPS.Output.ConfigVoltage", [](UPSData& d, double v) { d.outputVoltageNominal = (uint16_t)v; } },
+        
+        { "UPS.PowerConverter.Output.LowVoltageTransfer", [](UPSData& d, double v) { d.lowVoltageTransfer = (uint16_t)v; } },
+        { "UPS.Output.LowVoltageTransfer", [](UPSData& d, double v) { d.lowVoltageTransfer = (uint16_t)v; } },
+        { "UPS.Input.LowVoltageTransfer", [](UPSData& d, double v) { d.lowVoltageTransfer = (uint16_t)v; } },
+        
+        { "UPS.PowerConverter.Output.HighVoltageTransfer", [](UPSData& d, double v) { d.highVoltageTransfer = (uint16_t)v; } },
+        { "UPS.Output.HighVoltageTransfer", [](UPSData& d, double v) { d.highVoltageTransfer = (uint16_t)v; } },
+        { "UPS.Input.HighVoltageTransfer", [](UPSData& d, double v) { d.highVoltageTransfer = (uint16_t)v; } }
     };
 
     for (const auto& u : host->_hid_parser.getUsages()) {
